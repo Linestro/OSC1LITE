fe9f0b483fcc7a0ee4ffa89ddcc47de475186bf8
病名は愛 <yehaojie@umich.edu>
2018-12-21 10:41:26 -0800

    add wire 0x22 check mode bits

--- project_LITE_Vivado/17.srcs/sources_1/imports/imports/OSC1_LITE_one_channel_control.v
+++ project_LITE_Vivado/17.srcs/sources_1/imports/imports/OSC1_LITE_one_channel_control.v
@@ -42,7 +42,7 @@ wire [15:0]	data_from_user;
 
 wire [15:0] sdo;
 
-assign led = rst ? 8'b10101010 : {6'b111111,mode};
+assign led = rst ? 8'b10101010 : {6'b111111,~mode};
 
 spi_controller dac_spi(
 	.clk(clk),	// Opal Kelly ti_clk
@@ -69,13 +69,14 @@ okHost okHI(
 	.hi_in(hi_in), .hi_out(hi_out), .hi_inout(hi_inout), .hi_aa(hi_aa), .ti_clk(ti_clk),
 	.ok1(ok1), .ok2(ok2));
 
-wire [17*1-1:0]  ok2x;
-okWireOR # (.N(1)) wireOR (ok2, ok2x);
+wire [17*2-1:0]  ok2x;
+okWireOR # (.N(2)) wireOR (ok2, ok2x);
 okWireIn     wi00 (.ok1(ok1),                           .ep_addr(8'h00), .ep_dataout({sys_ctrl_pad1,rst}));
 okWireIn     wi01 (.ok1(ok1),                           .ep_addr(8'h01), .ep_dataout({sys_ctrl_pad2,mode}));
 okWireIn     wi02 (.ok1(ok1),                           .ep_addr(8'h02), .ep_dataout({sys_ctrl_pad3,clear_request}));
 okWireIn     wi03 (.ok1(ok1),                           .ep_addr(8'h03), .ep_dataout(data_from_user));
 
 okWireOut    wo21 (.ok1(ok1), .ok2(ok2x[ 0*17 +: 17 ]), .ep_addr(8'h21), .ep_datain(sdo));
+okWireOut    wo22 (.ok1(ok1), .ok2(ok2x[ 1*17 +: 17 ]), .ep_addr(8'h22), .ep_datain({14'b0,mode}));
 
 endmodule
